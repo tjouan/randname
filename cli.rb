a616013c8221e0007b02a83dce646754d82f0aec
Thibault Jouan <tj@klamath.a13.fr>
2009-12-29 16:20:34 +0000

    added a prototype of CLI version, currently for testing purpose

---
+++ cli.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+# Randname: a random word generator
+# Thibault Jouan <tj@a13.fr>
+
+libdir = File.join(File.expand_path(File.dirname(__FILE__)), './lib')
+$LOAD_PATH.unshift libdir unless $LOAD_PATH.include? libdir
+
+require 'randname'
+r = Randname.new
+
+puts r.name 8, true
