a5d94e1abea0cca1be815ce109f9da27bc19e9a5
Thibault Jouan <tj@klamath.a13.fr>
2009-12-29 15:01:52 +0000

    OO rewrite

--- ui.rb
+++ ui.rb
@@ -9,22 +9,34 @@ libdir = File.join(File.expand_path(File.dirname(__FILE__)), './lib')
 $LOAD_PATH.unshift libdir unless $LOAD_PATH.include? libdir
 
 require 'randname'
-r = Randname.new
+$r = Randname.new
 
 include Fox
 
-app = FXApp.new
+class RNGuiWindow < FXMainWindow
+  def initialize app
+    super app,
+      "Randname: a random word generator, GUI version",
+      :opts => DECOR_ALL
 
-win = FXMainWindow.new app, 'Randname: a random word generator'
+    FXToolTip.new self.getApp
 
-button_gen = FXButton.new win, 'Generate!'
-button_gen.connect SEL_COMMAND do |sender, selector, data|
-  puts r.name 8, true
+    button_gen = FXButton.new self, 'Generate!'
+    button_gen.connect SEL_COMMAND do |sender, selector, data|
+      puts $r.name 8, true
+    end
+    button_gen.tipText = 'Generate words based on your settings'
+  end
+
+  def create
+    super
+    show(PLACEMENT_SCREEN)
+  end
 end
-button_gen.tipText = 'Generate words based on your settings'
 
-FXToolTip.new app
-app.create
-
-win.show
-app.run
+if __FILE__ == $0
+  app = FXApp.new('RNGui', 'A13')
+  RNGuiWindow.new(app)
+  app.create
+  app.run
+end
