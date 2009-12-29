25a4a9992f137d7aa4772a06f28a6d9983704b0a
Thibault Jouan <tj@klamath.a13.fr>
2009-12-29 14:43:04 +0000

    using fxruby GUI toolkit, added a button to generate words

--- ui.rb
+++ ui.rb
@@ -2,11 +2,26 @@
 # Randname: a random word generator
 # Thibault Jouan <tj@a13.fr>
 
+require 'rubygems'
+require 'fox16'
+
 libdir = File.join(File.expand_path(File.dirname(__FILE__)), './lib')
 $LOAD_PATH.unshift libdir unless $LOAD_PATH.include? libdir
 
 require 'randname'
-
 r = Randname.new
-puts r.name 8
-puts r.name 8, true
+
+include Fox
+
+app = FXApp.new
+
+win = FXMainWindow.new app, 'Randname: a random word generator'
+button_gen = FXButton.new win, 'Generate!'
+button_gen.connect SEL_COMMAND do |sender, selector, data|
+  puts r.name 8, true
+end
+app.create
+
+win.show
+
+app.run
